29a30e090e917dd0f7a37249fa76226be8fba752
YosriElhadj <yosri.elhadj@esprit.tn>
2024-05-13 18:53:35 +0100

    fixed issues

--- src/main/java/com/example/pidev/test/Main.java
+++ src/main/java/com/example/pidev/test/Main.java
@@ -31,8 +31,8 @@ public  class Main  extends Application {
             //FXMLLoader loader = new FXMLLoader(getClass().getResource("/clientView.fxml"));
             //FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/pidev/hello-view.fxml"));
             //FXMLLoader loader = new FXMLLoader(getClass().getResource("/Fxml/Back.fxml"));
-            //FXMLLoader loader = new FXMLLoader(getClass().getResource("/Fxml/listass.fxml"));
-            //FXMLLoader loader = new FXMLLoader(getClass().getResource("/Fxml/listass.fxml"));
+           // FXMLLoader loader = new FXMLLoader(getClass().getResource("/Fxml/listass.fxml"));
+           // FXMLLoader loader = new FXMLLoader(getClass().getResource("/Fxml/listass.fxml"));
          FXMLLoader loader = new FXMLLoader(getClass().getResource("/adminDashboard.fxml"));
 
             Parent root = loader.load();
